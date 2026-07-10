41e606bea9d84aa606fb812e338648a8d2a7cba3
Sakshi <sakshisharma60058@gmail.com>
2026-07-10 09:54:44 +0530

    Add day 6

---
+++ 25ESKCS303/Day 6/script.js
@@ -0,0 +1,24 @@
+ let students =[
+       {
+           
+             name:'sakshi',
+            active:true,
+            branch: 'CS',
+           rollno  :34,
+            marks:99
+          },
+
+
+        
+            {
+            name: 'ritu',
+            branch :'CS',
+            rollno :300,
+           marks :98
+            }
+       ]
+       console.log( JSON.stringify(students))
+      let student=  [{"name":"sakshi","active":true,"branch":"CS","rollno":34,"marks":99},{"name":"ritu","branch":"CS","rollno":300,"marks":98}]
+      console.log(JSON.parse)
+
+        
