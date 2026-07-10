67fea451629734532ce1662888a4a929666a6679
Sakshi <sakshisharma60058@gmail.com>
2026-07-10 21:52:21 +0530

    Add day 8

---
+++ 25ESKCS303/Day 8/register.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <form action="process.php" method="POST">
+    
+        <input type="text" name ="name" placeholder="enter your name">
+        <br><br>
+         <input type="text" name ="college" placeholder="enter your college">
+         <br><br>
+         <input type="text" name ="branch" placeholder="enter your branch"><br><br>
+        <a class="btn btn-primary" href="#" role="button"></a>
+<button class="btn btn-primary" type="submit">Submit</button>
+</body>
+</html>
